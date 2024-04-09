c6f18f1778ff688ccc8bdd81a0792eb092fea5ab
Palak kumar <116839013+palakkumar998@users.noreply.github.com>
2024-04-10 01:06:19 +0530

    Hero Section Completed Part 2 and navbar setup

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -1,69 +1,90 @@
+'use client'
 import Image from 'next/image'
 import React from 'react'
 import profile from '/public/profile.JPG'
+import { TypeAnimation } from 'react-type-animation'
 
 const HeroSection = () => {
 	return (
-		<section>
-			<div className="grid grid-cols-1 sm:grid-cols-12">
-				<div className="col-span-7 place-self-center">
-					<h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-8xl lg:leading-normal font-extrabold">
-						Palak
+		<section className="container mx-auto px-4 sm:px-6 lg:px-8">
+			<div className="grid grid-cols-1 sm:grid-cols-2 gap-8 items-center">
+				<div className="text-center sm:text-left">
+					<h1 className="text-white mb-4 text-xl sm:text-5xl lg:text-6xl lg:leading-normal font-extrabold">
+						<span className="text-transparent bg-clip-text bg-gradient-to-r from-violet-600 to-orange-500">
+							Hello, I'm Palak
+						</span>
+						<br></br>
+						<TypeAnimation
+							sequence={[
+								// Same substring at the start will only be typed out once, initially
+								'Video Editor',
+								1000, // wait 1s before replacing "Mice" with "Hamsters"
+								'Graphic Designer',
+								1000,
+								'Web Developer',
+								1000,
+								'Cinematographer',
+								1000,
+							]}
+							wrapper="span"
+							speed={50}
+							repeat={Infinity}
+						/>
 					</h1>
-					<p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
+					<p className="text-[#ADB7BE] text-base sm:text-lg lg:text-xl mt-4">
 						Lorem ipsum dolor sit amet consectetur adipisicing elit.
 						Eveniet commodi, ad asperiores est doloremque nisi
 						sapiente obcaecati officiis a odit nostrum alias
 						perferendis necessitatibus, similique facilis culpa
 						ipsa, minima cum?
 					</p>
-					<div className="flex mx-8 my-2 gap-6">
+					<div className="flex flex-col sm:flex-row mt-6 sm:mt-8 space-y-4 sm:space-y-0 sm:space-x-4">
 						<button>
 							<a
 								href="#_"
-								class="relative inline-flex items-center justify-center p-4 px-5 py-3 overflow-hidden font-medium text-indigo-600 transition duration-300 ease-out rounded-full shadow-xl group hover:ring-1 hover:ring-purple-500"
+								className="relative inline-flex items-center justify-center px-6 py-3 overflow-hidden font-medium text-indigo-600 transition duration-300 ease-out rounded-full shadow-xl group hover:ring-1 hover:ring-purple-500 bg-gradient-to-br from-blue-600 via-purple-600 to-pink-700"
 							>
-								<span class="absolute inset-0 w-full h-full bg-gradient-to-br from-blue-600 via-purple-600 to-pink-700"></span>
-								<span class="absolute bottom-0 right-0 block w-64 h-64 mb-32 mr-4 transition duration-500 origin-bottom-left transform rotate-45 translate-x-24 bg-pink-500 rounded-full opacity-30 group-hover:rotate-90 ease"></span>
-								<span class="relative text-white">Hire Me</span>
+								<span className="absolute inset-0 w-full h-full bg-gradient-to-br from-blue-600 via-purple-600 to-pink-700"></span>
+								<span className="relative text-white">
+									Hire Me
+								</span>
 							</a>
 						</button>
 						<button>
 							<a
 								href="#_"
-								class="relative inline-flex items-center px-12 py-3 overflow-hidden text-lg font-medium text-purple-400 border-2 border-purple-500 rounded-full hover:text-white group hover:bg-gray-50"
+								className="relative inline-flex items-center px-8 py-3 overflow-hidden text-lg font-medium text-purple-400 border-2 border-purple-500 rounded-full hover:text-white group hover:bg-gray-50"
 							>
-								<span class="absolute left-0 block w-full h-0 transition-all bg-purple-600 opacity-100 group-hover:h-full top-1/2 group-hover:top-0 duration-400 ease"></span>
-								<span class="absolute right-0 flex items-center justify-start w-10 h-10 duration-300 transform translate-x-full group-hover:translate-x-0 ease">
+								<span className="absolute left-0 block w-full h-0 transition-all bg-purple-600 opacity-100 group-hover:h-full top-1/2 group-hover:top-0 duration-400 ease"></span>
+								<span className="absolute right-0 flex items-center justify-start w-10 h-10 duration-300 transform translate-x-full group-hover:translate-x-0 ease">
 									<svg
-										class="w-5 h-5"
+										className="w-5 h-5"
 										fill="none"
 										stroke="currentColor"
 										viewBox="0 0 24 24"
 										xmlns="http://www.w3.org/2000/svg"
 									>
 										<path
-											stroke-linecap="round"
-											stroke-linejoin="round"
-											stroke-width="2"
+											strokeLinecap="round"
+											strokeLinejoin="round"
+											strokeWidth="2"
 											d="M14 5l7 7m0 0l-7 7m7-7H3"
 										></path>
 									</svg>
 								</span>
-								<span class="relative">Download CV</span>
+								<span className="relative">Download CV</span>
 							</a>
 						</button>
 					</div>
 				</div>
-
-				<div className="col-span-5 place-self-center">
-					<div className="rounded-lg bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
+				<div className="text-center sm:text-right">
+					<div className="rounded-lg bg-[#181818] w-full max-w-sm sm:max-w-md lg:max-w-lg mx-auto lg:mx-0">
 						<Image
 							src={profile}
 							alt="My Profile Picture"
-							width={400}
-							height={400}
-							className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+							width={500}
+							height={500}
+							className="rounded-lg"
 						/>
 					</div>
 				</div>
