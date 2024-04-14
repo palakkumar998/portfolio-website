5a5fc9f76cb63dc3e5214a12fdb2cfacf06ebf16
Palak kumar <116839013+palakkumar998@users.noreply.github.com>
2024-04-15 00:33:51 +0530

    Adding icons and custom css in aboutSection

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -2,7 +2,16 @@
 import React, { useTransition, useState } from 'react'
 import Image from 'next/image'
 import TabButton from './TabButton'
-import  Aboutus  from '/public/images/Aboutus.jpg'
+import Aboutus from '/public/images/Aboutus.jpg'
+import dr from '/public/images/logo/1.png'
+import pr from '/public/images/logo/2.png'
+import ae from '/public/images/logo/3.png'
+import ps from '/public/images/logo/4.png'
+import ai from '/public/images/logo/5.png'
+import cd from '/public/images/logo/6.png'
+import yt from '/public/images/logo/7.png'
+import li from '/public/images/logo/8.png'
+import insta from '/public/images/logo/9.png'
 
 const TAB_DATA = [
 	{
@@ -45,9 +54,29 @@ const TAB_DATA = [
 		id: 'platforms',
 		content: (
 			<ul className="list-disc pl-2">
-				<li>YouTube</li>
-				<li>Instagram</li>
-				<li>LinkedIn</li>
+				<div className="flex mb-2 py-3 gap-6 items-center">
+					<Image
+						src={yt}
+						alt="davinci"
+						width={60}
+						height={60}
+						className="transition-transform duration-300 hover:scale-110"
+					/>
+					<Image
+						src={li}
+						alt="davinci"
+						width={50}
+						height={50}
+						className="transition-transform duration-300 hover:scale-110"
+					/>
+					<Image
+						src={insta}
+						alt="davinci"
+						width={50}
+						height={50}
+						className="transition-transform duration-300 hover:scale-110"
+					/>
+				</div>
 			</ul>
 		),
 	},
@@ -55,12 +84,56 @@ const TAB_DATA = [
 		title: 'Tools',
 		id: 'tools & technologies',
 		content: (
-			<ul className="list-disc pl-2">
-				<li>Davinci Resolve</li>
+			<ul className="list-disc grid-cols-2 pl-2">
+				<div className="flex mb-2 py-3 gap-3 items-center">
+					<Image
+						src={dr}
+						alt="davinci"
+						width={80}
+						height={80}
+						className="transition-transform duration-300 hover:scale-110"
+					/>
+					<Image
+						src={pr}
+						alt="premire"
+						width={80}
+						height={80}
+						className="transition-transform duration-300 hover:scale-110"
+					/>
+					<Image
+						src={ae}
+						alt="after effects"
+						width={80}
+						height={80}
+						className="transition-transform duration-300 hover:scale-110"
+					/>
+					<Image
+						src={ps}
+						alt="photoshop"
+						width={80}
+						height={80}
+						className="transition-transform duration-300 hover:scale-110"
+					/>
+					<Image
+						src={ai}
+						alt="illustrator"
+						width={80}
+						height={80}
+						className="transition-transform duration-300 hover:scale-110"
+					/>
+					<Image
+						src={cd}
+						alt="corel draw"
+						width={60}
+						height={60}
+						className="transition-transform duration-300 hover:scale-110"
+					/>
+				</div>
+				{/* <li>Davinci Resolve</li>
 				<li>AdobePremire Pro</li>
 				<li>Adobe After Effects</li>
 				<li>Adobe Illustrator</li>
-				<li>Corel Draw</li>
+				<li>Corel Draw</li> */}
 			</ul>
 		),
 	},
@@ -79,14 +152,20 @@ const AboutSection = () => {
 	return (
 		<section className="text-white" id="about">
 			<div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
-				<Image src={Aboutus} alt="image" width={500} height={500} />
+				<Image
+					src={Aboutus}
+					alt="image"
+					width={600}
+					height={600}
+					className="rounded-xl spotlight"
+				/>
 				<div className="mt-4 md:mt-0 text-left flex flex-col h-full">
 					<h2 className="text-4xl font-bold text-white mb-4">
 						About Me
 					</h2>
 					<p className="text-base lg:text-lg">
 						With over 3 years of experience in video editing and
-						content creation, I've honed my ability to produce
+						content creation, I have honed my ability to produce
 						engaging YouTube videos, Instagram reels, posts, and
 						flyers. My strong technical foundation, backed by a
 						B.Tech degree in Computer Science and Engineering from
