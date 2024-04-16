9512532c039fcc7c29617db5c5752f0f23b289f3
Palak kumar <116839013+palakkumar998@users.noreply.github.com>
2024-04-17 01:07:55 +0530

    Project Tag completion

--- src/app/components/ProjectCard.jsx
+++ src/app/components/ProjectCard.jsx
@@ -6,7 +6,7 @@ const ProjectCard = ({ imgUrl, title, description, gitUrl, previewUrl }) => {
 	return (
 		<div>
 			<div
-				className="h-52 md:h-72 rounded-t-xl relative group"
+				className="h-52 md:h-72 rounded-t-xl relative group "
 				style={{
 					background: `url(${imgUrl})`,
 					backgroundSize: 'cover',
@@ -29,7 +29,7 @@ const ProjectCard = ({ imgUrl, title, description, gitUrl, previewUrl }) => {
 			</div>
 			<div className="text-white rounded-b-xl mt-3 bg-[#181818]py-6 px-4">
 				<h5 className="text-xl font-semibold mb-2">{title}</h5>
-				<p className="text-[#ADB7BE]">{description}</p>
+				<p className="text-gray-500 ">{description}</p>
 			</div>
 		</div>
 	)
--- src/app/components/ProjectSection.jsx
+++ src/app/components/ProjectSection.jsx
@@ -1,71 +1,103 @@
 'use client'
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import ProjectCard from './ProjectCard'
+import ProjectTag from './ProjectTag'
 const projectsData = [
 	{
 		id: 1,
 		title: 'Its Essential Tech',
-		description: 'Project 1 description',
+		description: 'Interactive Thumbnails Designing',
 		image: '/images/projects/1.png',
-		tag: ['All', 'Web'],
+		tag: ['All', 'Designing'],
 		gitUrl: '/',
 		previewUrl: 'https://youtube.com/@ItsEssentialTech?si=fzhkK6qv_uw2Tip2',
 	},
 	{
 		id: 2,
 		title: 'Karma Yogi',
-		description: 'Project 2 description',
+		description: 'Yoga Teacher Instagram reels',
 		image: '/images/projects/2.png',
-		tag: ['All', 'Web'],
+		tag: ['All', 'Editing'],
 		gitUrl: '/',
-		previewUrl: 'https://www.instagram.com/karma_yogiji?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==',
+		previewUrl:
+			'https://www.instagram.com/karma_yogiji?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==',
 	},
 	{
 		id: 3,
 		title: 'Ieva Simtniece',
-		description: 'Project 3 description',
+		description: 'Talking Head Instagram reels',
 		image: '/images/projects/3.png',
-		tag: ['All', 'Web'],
+		tag: ['All', 'Editing'],
 		gitUrl: '/',
-		previewUrl: 'https://www.instagram.com/ievayog?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==',
+		previewUrl:
+			'https://www.instagram.com/ievayog?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==',
 	},
 	{
-		id: 3,
-		title: 'Podcasts',
-		description: 'Youtube channel',
+		id: 4,
+		title: 'Ieva Yoga',
+		description: 'Podcasts Editing',
 		image: '/images/projects/4.png',
-		tag: ['All', 'Web'],
+		tag: ['All', 'Editing'],
 		gitUrl: '/',
 		previewUrl: 'https://youtu.be/VHyEEIm64xU?si=AIBHHkYUVb89u_pq',
 	},
 	{
-		id: 3,
+		id: 5,
 		title: 'Rishikesh Yogakulam',
-		description: 'InstaGram',
+		description: 'Instagram Post Designing',
 		image: '/images/projects/5.png',
-		tag: ['All', 'Web'],
+		tag: ['All', 'Designing'],
 		gitUrl: '/',
 		previewUrl: '/',
 	},
 	{
-		id: 3,
+		id: 6,
 		title: 'Yoga With master D',
-		description: 'InstaGram',
+		description: 'Youtube Thumbnails Designing',
 		image: '/images/projects/6.png',
-		tag: ['All', 'Web'],
+		tag: ['All','Designing'],
 		gitUrl: '/',
 		previewUrl: '/',
 	},
 ]
 
 const ProjectSection = () => {
+	const [tag, setTag] = useState('All')
+	const ref = useRef(null)
+
+	const handleTagChange = (newTag) => {
+		setTag(newTag)
+	}
+	const filteredProjects = projectsData.filter((project) =>
+		project.tag.includes(tag)
+	)
+
 	return (
 		<section id="projects">
 			<h2 className="text-center text-4xl font-bold text-white mt-4 mb-8 md:mb-12">
-				See my Work
+				My Clients Work
 			</h2>
+			{/*  section for category button  */}
+			<div className="text-white flex flex-row justify-center items-center gap-4 py-6">
+				<ProjectTag
+					onClick={handleTagChange}
+					name="All"
+					isSelected={tag === 'All'}
+				/>
+				<ProjectTag
+					onClick={handleTagChange}
+					name="Editing"
+					isSelected={tag === 'Editing'}
+				/>
+				<ProjectTag
+					onClick={handleTagChange}
+					name="Designing"
+					isSelected={tag === 'Designing'}
+				/>
+			</div>
+			{/*  section for category button  ends here */}
 			<div className="grid md:grid-cols-3 gap-8 md:gap-12">
-				{projectsData.map((project) => (
+				{filteredProjects.map((project) => (
 					<ProjectCard
 						key={project.id}
 						title={project.title}
